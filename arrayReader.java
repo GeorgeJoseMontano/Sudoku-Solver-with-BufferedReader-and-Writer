4259a88ee7d77d6fe65dcd7d3727aca2c6f99d7c
George Jose Montano <yoshilute.248@gmail.com>
2021-04-15 17:00:50 +0800

    Update arrayReader.java

--- arrayReader.java
+++ arrayReader.java
@@ -1,132 +1,123 @@
 import java.io.*;
 
 //test if change
-public class arrayReader{
+public class arrayReader
+{	 
 	 public static boolean isSafe(int[][] board,
              int row, int col,
              int num)
 		{
 		// Row has the unique (row-clash)
 		for (int d = 0; d < board.length; d++)
-		{
-		
-		// Check if the number we are trying to
-		// place is already present in
-		// that row, return false;
-		if (board[row][d] == num) {
-		return false;
-		}
+		{			
+			// Check if the number we are trying to
+			// place is already present in
+			// that row, return false;
+			if (board[row][d] == num) {
+			return false;
+			}
 		}
 		
 		// Column has the unique numbers (column-clash)
 		for (int r = 0; r < board.length; r++)
-		{
-		
+		{		
 		// Check if the number
 		// we are trying to
 		// place is already present in
 		// that column, return false;
-		if (board[r][col] == num)
-		{
-		return false;
-		}
+			if (board[r][col] == num)
+			{
+				return false;
+			}
 		}
 		
 		// Corresponding square has
 		// unique number (box-clash)
-		int sqrt = (int)Math.sqrt(board.length);
-		int boxRowStart = row - row % sqrt;
-		int boxColStart = col - col % sqrt;
-		
-		for (int r = boxRowStart;
-		r < boxRowStart + sqrt; r++)
+		row = (row/(board.length/3)*(board.length/3));
+		col = (col/3)*3;
+		for(int r=0; r<(board.length/3); r++)
 		{
-		for (int d = boxColStart;
-		d < boxColStart + sqrt; d++)
-		{
-		if (board[r][d] == num)
-		{
-		return false;
-		}
-		}
+			for(int c=0; c<3; c++)
+			{
+				if(board[row+r][col+c] == num)
+					 return false ;
+			}
 		}
 		
 		// if there is no clash, it's safe
 		return true;
 		}
 		
-		public static boolean solveSudoku(
-		int[][] board, int n)
+		public static boolean solveSudoku(int[][] board, int n)
 		{
-		int row = -1;
-		int col = -1;
-		boolean isEmpty = true;
-		for (int i = 0; i < n; i++)
-		{
-		for (int j = 0; j < n; j++)
-		{
-		if (board[i][j] == 0)
-		{
-		row = i;
-		col = j;
-		
-		// We still have some remaining
-		// missing values in Sudoku
-		isEmpty = false;
-		break;
-		}
-		}
-		if (!isEmpty) {
-		break;
-		}
-		}
-		
-		// No empty space left
-		if (isEmpty)
-		{
-		return true;
-		}
-		
-		// Else for each-row backtrack
-		for (int num = 1; num <= n; num++)
-		{
-		if (isSafe(board, row, col, num))
-		{
-		board[row][col] = num;
-		if (solveSudoku(board, n))
-		{
-		// print(board, n);
-		return true;
-		}
-		else
-		{
-		// replace it
-		board[row][col] = 0;
-		}
-		}
-		}
-		return false;
+			int row = -1;
+			int col = -1;
+			boolean isEmpty = true;
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					if (board[i][j] == 0)
+					{
+						row = i;
+						col = j;
+						// We still have some remaining
+						// missing values in Sudoku
+						isEmpty = false;
+						break;
+					}
+				}
+				if (!isEmpty) 
+				{
+					break;
+				}
+			}
+			
+			// No empty space left
+			if (isEmpty)
+			{
+				return true;
+			}
+			
+			// Else for each-row backtrack
+			for (int num = 1; num <= n; num++)
+			{
+				if (isSafe(board, row, col, num))
+				{
+					board[row][col] = num;
+					if (solveSudoku(board, n))
+					{
+						// print(board, n);
+						return true;
+					}
+					else
+					{
+						// replace it
+						board[row][col] = 0;
+					}
+				}
+			}
+			return false;
 		}
 		
 		public static void print(
 		int[][] board, int N)
 		{
-		
-		// We got the answer, just print it
-		for (int r = 0; r < N; r++)
-		{
-		for (int d = 0; d < N; d++)
-		{
-		System.out.print(board[r][d]);
-		System.out.print(" ");
-		}
-		System.out.print("\n");
-		
-		if ((r + 1) % (int)Math.sqrt(N) == 0)
-		{
-		System.out.print("");
-		}
-		}
+			// We got the answer, just print it
+			for (int r = 0; r < N; r++)
+			{
+				for (int d = 0; d < N; d++)
+				{
+					System.out.print(board[r][d]);
+					System.out.print(" ");
+				}
+				System.out.print("\n");
+				
+				if ((r + 1) % (int)Math.sqrt(N) == 0)
+				{
+					System.out.print("");
+				}
+			}
 		}
 	
 	public static void main(String args[])
